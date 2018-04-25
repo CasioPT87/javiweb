5a840c2d07ceaeed5995aedb11225c64c5756ee9
Serch <sergioibanezmoreno@gmail.com>
2018-04-25 22:02:46 +0100

    yippieeee, ya estas los static files y estos subidos a aws. faltan los media

--- javiweb/settings.py
+++ javiweb/settings.py
@@ -137,7 +137,7 @@ MEDIA_ROOT = 'images'
 MEDIA_URL = 'media/'
 
 # is the folder where every static files will be stored after a manage.py collectstatic
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
@@ -146,13 +146,18 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_LOCATION = 'static/admin/img'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'mystatic'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+AWS_LOCATION = 'static'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+#permite a boto3 subir tus static files a AWS con el collectstatics
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 
